8307164aba2cac503a094f32a47e4e1aadb16ac5
Mahamadou19-75 <gadiarramahamadou@gmail.com>
2026-02-05 17:27:29 +0100

    task7rec

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
-"""
-
-This module prints the name of a anything
-
-Output: 'My name is <first name> <last name>'
-
-    * The first name and last name must be a strings
-
-"""
+"""This module provides a function that prints a formatted name."""
 
 
 def say_my_name(first_name, last_name=""):
-    """
+    """Print 'My name is <first_name> <last_name>'."""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    # Supprime les espaces superflus si last_name est vide
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
 
-    Prints the names
-
-    Args:
-        first_name (str): The first name of anything.
-        last_name (str): The last name of anything.
-
-    Raises:
-        TypeError: If `first_name` and `last_name` aren't strings.
-
-    """
-
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-
-    print('My name is', first_name, last_name)
