6bba6a244308ab2a862f9d13c3507ab1769770d2
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-03-27 15:39:16 +0100

    task6

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,12 @@
+const character = document.querySelector('#character');
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    character.textContent = data.name;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
